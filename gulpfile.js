656e90ce7ef92ae0f53f13a3064c7e546332f4c8
elnara.kozlitina <elnarakozlitina@Tony.local>
2014-08-23 01:37:18 -0400

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by elnarakozlitina on 8/23/14.
+ */
+'use strict';
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  sass = require('gulp-ruby-sass'),
+  karma = require('gulp-karma');
