3a81f1f023a274adeab8ed9ced0df327177b66c9
Elnara Kozlitina <elle@davinci.local>
2014-11-03 22:04:07 -0500

    parse js structure, ability to parse integers

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,93 @@
 /**
  * Created by elle on 11/3/14.
  */
+
+/* jshint globalstrict: true */
+
+'use strict';
+
+function parse(expr) {
+  var lexer = new Lexer(),
+      parser = new Parser(lexer);
+
+  return parser.parse(expr);
+}
+
+
+/**
+ *The lexer takes the original expression string and returns
+ * a collection of tokens parsed from that string.
+ */
+
+function Lexer() {
+
+}
+
+Lexer.prototype.lex = function lex(text) {
+  this.text = text || '';
+  this.index = 0;
+  this.ch = undefined;
+  this.tokens = [];
+
+  while (this.index < this.text.length) {
+    this.ch = this.text.charAt(this.index);
+    if (this.isNumber(this.ch)) {
+      this.readNumber();
+    } else {
+      throw 'Unexpected next character:' + this.ch;
+    }
+  }
+
+  return this.tokens;
+};
+
+Lexer.prototype.isNumber = function isNumber(ch) {
+  return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function readNumber () {
+  var ch, number = '';
+
+  while (this.index < this.text.length) {
+    ch = this.text.charAt(this.index);
+    if (this.isNumber(ch)) {
+      number += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+
+  number = +number;
+  this.tokens.push({
+    text: number,
+    fn: _.constant(number),
+    constant: true
+  });
+};
+
+/**
+ * The parser takes the token collection produced by
+ * the lexer and returns a function that evaluates the expression in a given context.
+ */
+
+function Parser(lexer) {
+  this.lexer = lexer;
+}
+
+Parser.prototype.parse = function parse(text) {
+  this.tokens = this.lexer.lex(text);
+  return this.primary();
+};
+
+Parser.prototype.primary = function primary() {
+  var token = this.tokens[0],
+      primary = token.fn;
+
+  if (token.constant) {
+    primary.constant = true;
+    primary.literal = true;
+  }
+
+  return primary;
+};
--- tests/parse.spec.js
+++ tests/parse.spec.js
@@ -1,3 +1,22 @@
 /**
  * Created by elle on 11/3/14.
  */
+
+/* jshint globalstrict: true */
+/* global parse: false */
+
+describe('parse', function() {
+
+  it('can parse integer', function() {
+    var fn = parse('42');
+    expect(fn).toBeDefined();
+    expect(fn()).toBe(42);
+  });
+
+  it('makes integers both literal and constant', function () {
+    var fn = parse('42');
+    expect(fn.constant).toBe(true);
+    expect(fn.literal).toBe(true);
+  });
+
+});
